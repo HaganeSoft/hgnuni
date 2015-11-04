28cb2cf155ea75053fe8c32b4cd402150d86bf3c
Erick Castillo de la Garza <ecastillo550@gmail.com>
2015-11-04 14:38:59 -0600

    se instalo hagane framework y se modifico para que viva con otras apps <3

---
+++ app-uni/Controller/User.php
@@ -0,0 +1,38 @@
+<?php
+namespace Hagane\Controller;
+
+class User extends AbstractController{
+	function _init() {
+		echo $this->db->database_log['error'];
+	}
+
+	function index() {
+	}
+
+	function auth() {
+		if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+			$this->auth->authenticate($_POST['user'], $_POST['password']);
+			if ($this->auth->isAuth()) {
+				$this->user = new \Hagane\Model\User($this->auth, $this->db);
+				if ($this->user->getUserType() == 'Administrador') {
+					header("Location:" . $this->config['document_root'] . "Admin/index");
+					die();
+				}
+				if ($this->user->getUserType() == 'Cliente') {
+					header("Location:" . $this->config['document_root'] . "Cliente/index");
+					die();
+				}
+				if ($this->user->getUserType() == 'Doctor') {
+					header("Location:" . $this->config['document_root'] . "Doctor/index");
+					die();
+				}
+			}
+		}
+	}
+
+	function logout() {
+		$this->auth->logout();
+	}
+}
+
+?>
---
+++ app-uni/Model/UserModel.php
@@ -0,0 +1,8 @@
+<?php
+namespace Hagane\Model;
+
+class User {
+
+}
+
+?>
