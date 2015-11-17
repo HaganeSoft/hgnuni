93193dc07c724158c78bb09e7e723a448c5dd27d
Javier Meza <javenmeza@gmail.com>
2015-11-17 05:45:25 -0600

    Arreglo del header y footer

--- app-uni/config/config.php
+++ app-uni/config/config.php
@@ -17,11 +17,11 @@ class Config {
 				'template' => 'main',
 				'db_engine' => 'mysql',
 				'db_server' => 'localhost',
-				'db_database' => 'hguni',
-				'db_user' => 'hagane_dev',
-				'db_password' => '',
+				'db_database' => 'hgnuni',
+				'db_user' => 'root',
+				'db_password' => 'Guapuras95!',
 				'session_time' => 3600,
-				'document_root' => '/hgnuni/public_html/'
+				'document_root' => '/hagane/hgnuni/'
 			);
 	}
 
