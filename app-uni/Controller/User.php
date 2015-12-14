d080e0e2be08e464805c73faf75e36c5c379f3a6
Julio Aguilar <julioc.aguilarv@gmail.com>
2015-12-13 19:47:33 -0600

    Refactoriza Controller de Users y User y Models, implementa funcionalidad de sign up y sign in a db

--- app-uni/Controller/User.php
+++ app-uni/Controller/User.php
@@ -3,10 +3,12 @@ namespace Hagane\Controller;
 
 class User extends AbstractController{
 	function _init() {
+		/*if (!$this->auth->isAuth()) {
+		             header("Location:" . $this->config['document_root'] . "index");
+		             die();
+		        }*/
+		include_once($this->config['appPath'].'Model/UserModel.php');
 		echo $this->db->database_log['error'];
-	}
-
-	function index() {
 	}
 
 	function auth() {
@@ -21,6 +23,56 @@ class User extends AbstractController{
 
 	function logout() {
 		$this->auth->logout();
+	}
+
+	function index() {
+	}
+
+	function ajaxSetUser() {
+		$this->print_template = false;
+		$this->sendJson = true;
+		$this->userModel = new \Hagane\Model\User($this->auth, $this->db);
+
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$data = array(
+				'name' => $_POST['name'],
+				'country' => $_POST['country'],
+				'email' => $_POST['email'],
+				'address' => $_POST['address'],
+				'university' => $_POST['university'],
+				'phone' => $_POST['phone'],
+				'degree' => $_POST['degree']);
+			if( $this->userModel->setUser($data) != NULL ){
+				header("Location:" . $this->config['document_root'] . "index");
+			}
+		}
+		echo "error";
+	}
+
+	function ajaxUpdateUser() {
+		$this->sendJson = true;
+		$this->print_template = false;
+		$this->userModel = new \Hagane\Model\User($this->auth, $this->db);
+
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$data = array(
+				'id' => $_POST['id'],
+				'name' => $_POST['name'],
+				'country' => $_POST['country'],
+				'email' => $_POST['email'],
+				'address' => $_POST['address'],
+				'university' => $_POST['university'],
+				'phone' => $_POST['phone'],
+				'degree' => $_POST['degree'],
+				'password' => $_POST['password']);
+		}
+	}
+
+	function ajaxGetUser() {
+		$this->sendJson = true;
+		$this->print_template = false;
+		$this->userModel = new \Hagane\Model\User($this->auth, $this->db);
+		echo json_encode($this->userModel->getUser());
 	}
 
 	function sign_up() {
